815ff0db7fb99e235e4a24139f6e09a7dea72164
quoctran <quoctran@kms-technology.com>
2018-08-30 17:39:54 +0700

    update report

--- RunTest.bat
+++ RunTest.bat
@@ -2,9 +2,12 @@
 
 REM Set variables
 SET SOURCE_DIR="D:\Projects\Baby_Shark\perf\BabyPerformance"
+SET THREAD=%1
+SET RAMPUP=%2
+SET DURATION=%3
 SET CURRENT_DATETIME=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%.%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 SET JMX_Path="%SOURCE_DIR%\Perf_Training.jmx"
-SET LOG_PATH="%SOURCE_DIR%\log\log_%CURRENT_DATETIME%.xml"
+SET LOG_PATH="%SOURCE_DIR%\log\%CURRENT_DATETIME%_threads.%THREAD%.rampup.%RAMPUP%.duration.%DURATION%.xml"
 SET HTMLREPORT_PATH="%SOURCE_DIR%\htmlreport"
 
 REM Remove existing htmlreport directory
@@ -14,4 +17,4 @@ REM Go to jmeter bin directory
 cd %JMETER_HOME%\bin
 
 REM Run load test and generate html report
-jmeter -n -t %JMX_Path% -l %LOG_PATH% -e -o %HTMLREPORT_PATH% -Jduration=%1
+jmeter -n -t %JMX_Path% -l %LOG_PATH% -e -o %HTMLREPORT_PATH% -Jthreads=%THREAD% -Jrampup=%RAMPUP% -Jduration=%DURATION%
