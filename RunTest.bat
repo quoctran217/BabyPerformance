8b6f845f6f31a572dbdbff5691f3faa6e6d2050a
quoctran <quoctran@kms-technology.com>
2018-09-06 13:39:46 +0700

    update

--- RunTest.bat
+++ RunTest.bat
@@ -7,15 +7,15 @@ SET RAMPUP=%2
 SET DURATION=%3
 SET CURRENT_DATETIME=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%.%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 SET JMX_Path="%SOURCE_DIR%\Perf_Training.jmx"
-SET LOG_PATH="%SOURCE_DIR%\log\%CURRENT_DATETIME%_threads.%THREAD%.rampup.%RAMPUP%.duration.%DURATION%.jtl"
+SET LOG_PATH="%SOURCE_DIR%\log\%CURRENT_DATETIME%_threads.%THREAD%.rampup.%RAMPUP%.duration.%DURATION%.csv"
 SET HTMLREPORT_PATH="%SOURCE_DIR%\htmlreport"
 
 REM Remove existing htmlreport directory
 rmdir %HTMLREPORT_PATH% /s /q
 
 REM Go to jmeter bin directory
-cd %JMETER_HOME%\bin
+cd /d %JMETER_HOME%\bin
 
 REM Run load test and generate html report
 jmeter -n -t %JMX_Path% -l %LOG_PATH% -e -o %HTMLREPORT_PATH% -Jthreads=%THREAD% -Jrampup=%RAMPUP% -Jduration=%DURATION%
-::jmeter -g F:\BabySharks\git\BabyPerformance\log\20180904.141740_threads.3.rampup.3.duration.30.csv -e -o %HTMLREPORT_PATH%
+::jmeter -g F:\BabySharks\git\BabyPerformance\log\merge_result2.csv -e -o %HTMLREPORT_PATH%
